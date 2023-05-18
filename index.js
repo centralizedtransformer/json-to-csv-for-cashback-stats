da1281d37a741457e5da0e26a97c142eec37e02f
Andrew <andrew@atomicwallet.io>
2023-05-18 14:58:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,23 @@
 const { Parser, transforms: { unwind } } = require('json2csv');
 const fs = require('fs');
 
-let rawdata = fs.readFileSync('response-july-for-dev.json');
+let rawdata = fs.readFileSync('response-4.json');
 let data = JSON.parse(rawdata);
 
 const fields = [
   'id',
-  'orderId',
-  'atomicId',
   'status',
-  'fromCurrency',
-  'toCurrency',
-  'amountSend',
-  'amountReceive',
-  'payinAddress',
-  'payinExtraId',
-  'payoutAddress',
-  'payoutExtraId',
-  'refundAddress',
-  'refundExtraId',
-  'payinHash',
-  'payoutHash',
-  'createdAt',
-  'updatedAt',
-  'Cashbacks.id',
-  'Cashbacks.atomicId',
-  'Cashbacks.paymentId',
-  'Cashbacks.orderId',
+  'usdValue',
   'Cashbacks.bnbAddress',
   'Cashbacks.ethAddress',
   'Cashbacks.expectedCashbackAmount',
   'Cashbacks.awcBep2Balance',
-  'Cashbacks.awcBep2Rate',
-  'Cashbacks.exchangeService',
-  'Cashbacks.platform',
-  'Cashbacks.walletVersion',
-  'Cashbacks.order_id'
+  'Cashbacks.awcBep2Rate'
 ];
 const transforms = [unwind({ paths: ['Cashbacks'] })];
 const json2csvParser = new Parser({ fields, transforms });
 const csv = json2csvParser.parse(data);
-fs.writeFile('response.csv', csv, function (err) {
+fs.writeFile('response-4.csv', csv, function (err) {
   if (err) return console.log(err);
   console.log('wrote!');
 });
